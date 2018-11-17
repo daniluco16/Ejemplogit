aadb3a253c90a3edb3872fb5c46dcd5662de26f4
daniluco <daniel-dela@hotmail.com>
2018-11-17 17:21:34 +0100

    commit 2

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo "La prueba funciono";
+        echo "La prueba funciono correctamente";
         ?>
     </body>
 </html>
